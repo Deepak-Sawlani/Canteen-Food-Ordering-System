3eb3beb839a7175d9d3f5bd6f5c869adc39523da
Deepak Sawlani <deepaksawlani2003@gmail.com>
2026-05-05 18:03:42 +0530

    Final Fix: Connected MongoDB with process.env and updated CORS

--- api/server.js
+++ api/server.js
@@ -1,21 +1,32 @@
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const userRoutes = require("./routes/UserRoutes");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "https://canteen-food-ordering-system-dejc.vercel.app/",  // Yahan apna Vercel link paste karein
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
-mongoose.connect("process.env.MONGODB_URI", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
+}).then(() => console.log("MongoDB Connected..."))
+.catch(err => console.log("Database Connection Error: ", err));
 
 app.use("/api/menu", require("./routes/menuRoutes"));
 app.use("/api/order", require("./routes/orderRoutes"));
 app.use("/api/user", userRoutes);
 
+const PORT = process.env.PORT || 5000; 
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+//app.listen(5000, () => console.log("Server running on port 5000"));
 module.exports = app;
